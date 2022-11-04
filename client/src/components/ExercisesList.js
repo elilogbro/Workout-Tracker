880b6db4e1a5e5f670a5723f53818a0ef56f5ccf
Eli <elilogbro@gmail.com>
2022-11-04 13:45:54 -0400

    configured state for exercises added to new routine

--- client/src/components/ExercisesList.js
+++ client/src/components/ExercisesList.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import ExerciseListCard from './ExerciseListCard';
+
 import { ExercisesContainer, Wrapper } from '../styles/ExercisesListStyles';
 
 function ExercisesList() {
     const [exercises, setExercises] = useState(null)
+    const [exercisesForNewRoutine, setExercisesForNewRoutine] = useState([])
 
     useEffect(() => {
         fetch('/exercises')
@@ -15,10 +17,19 @@ function ExercisesList() {
         return <div>Loading...</div>
     }
 
+    const handleAddToNewRoutine = (exercise) => {
+        setExercisesForNewRoutine([...exercisesForNewRoutine, exercise])
+    }
+
+    const renderNewRoutine = exercisesForNewRoutine.length > 0 && exercisesForNewRoutine.map(exercise =>
+        <ExercisesCard key={exercise.id} exercise={exercise} />
+    )
+
     const renderExercises = exercises.map(exercise =>
         <ExerciseListCard
             exercise={exercise}
             key={exercise.id}
+            handleAddToNewRoutine={handleAddToNewRoutine}
         />
     )
 
@@ -29,6 +40,8 @@ function ExercisesList() {
             </ExercisesContainer>
             <div>
                 <h2>Current Routine</h2>
+                {exercisesForNewRoutine.length === 0 && <div>Add exercises!</div>}
+                {renderNewRoutine}
             </div>
         </Wrapper>
     )

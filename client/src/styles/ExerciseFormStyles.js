dbc07fa2dd24ed567c547c3ae267b06535074e67
Eli <+>
2023-01-27 14:33:00 -0500

    adjusted form style for mobile

--- client/src/styles/ExerciseFormStyles.js
+++ client/src/styles/ExerciseFormStyles.js
@@ -32,7 +32,7 @@ export const Form = styled.form`
     flex-direction: column;
     text-align: center;
     padding: 6vw;
-    width: 30vw;
+    width: fit-content;
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
 `;
 
--- client/src/styles/RoutineHistoryStyles.js
+++ client/src/styles/RoutineHistoryStyles.js
@@ -4,7 +4,7 @@ export const Routines = styled.div`
     display: flex;
     flex-direction: column;
     overflow-y: scroll;
-    width: 40vw;
+    width: 90%;
     height: 69vh;
     box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
     border: solid;
